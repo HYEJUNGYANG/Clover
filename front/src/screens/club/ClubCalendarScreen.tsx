b5c1c9687394b5ed63bf7d42fc185346ba8c0154
양혜정 <gpwjd248@gmail.com>
2024-08-30 16:29:27 +0900

    일정 등록 버튼 이미지 추가, 관리자일 때만 해당 버튼 나오도록 설정, 폰에서 폰트 크기 설정에 따라서 사이즈 달라지는 부분 해결

--- front/src/screens/club/ClubCalendarScreen.tsx
+++ front/src/screens/club/ClubCalendarScreen.tsx
@@ -1,16 +1,35 @@
 import ClubCalendarList from '@/components/club/ClubCalendarList';
 import {colors, styleValues} from '@/constants';
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {Pressable} from 'react-native';
+import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 interface ClubCalendarScreenProps {}
 
 function ClubCalendarScreen({}: ClubCalendarScreenProps) {
+  const insets = useSafeAreaInsets();
+  const isAdmin = false; // 임시
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.alignContainer}>
         <ClubCalendarList />
       </View>
+      {isAdmin && (
+        <Pressable
+          style={({pressed}) => [
+            styles.addButton,
+            {bottom: insets.bottom + styleValues.CONTAINER_MARGIN_HORIZONTAL},
+            pressed && {opacity: 0.6},
+          ]}>
+          <Image
+            resizeMode="contain"
+            source={require('@/assets/icon/club/calendar-add-icon.png')}
+            style={{width: 25, height: 26, marginLeft: 3, marginTop: 3}}
+          />
+        </Pressable>
+      )}
     </SafeAreaView>
   );
 }
@@ -24,6 +43,21 @@ const styles = StyleSheet.create({
     marginHorizontal: styleValues.CONTAINER_MARGIN_HORIZONTAL,
     gap: 12,
     marginVertical: 17,
+  },
+  addButton: {
+    position: 'absolute',
+    right: styleValues.CONTAINER_MARGIN_HORIZONTAL,
+    width: 56,
+    height: 56,
+    borderRadius: 56,
+    backgroundColor: colors.MAIN_GREEN,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: colors.BLACK,
+    shadowOffset: {width: 2, height: 2},
+    shadowRadius: 4,
+    shadowOpacity: 0.25,
+    elevation: 2,
   },
 });
 
--- front/src/screens/club/ClubMemberScreen.tsx
+++ front/src/screens/club/ClubMemberScreen.tsx
@@ -1,16 +1,27 @@
+import {colors, styleValues} from '@/constants';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 interface ClubMemberScreenProps {}
 
 function ClubMemberScreen({}: ClubMemberScreenProps) {
   return (
-    <View>
-      <Text>Member</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.alignContainer}>
+        <Text>Member</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.WHITE,
+  },
+  alignContainer: {
+    marginHorizontal: styleValues.CONTAINER_MARGIN_HORIZONTAL,
+  },
+});
 
 export default ClubMemberScreen;
