8a95b917b225ec4130b8abf72de99a5495bca459
양혜정 <gpwjd248@gmail.com>
2024-09-23 04:02:32 +0900

    Splash Screen 추가 (현재는 ios만 추가하고 이후 android에도 추가 예정)

--- front/src/navigations/root/RootNavigator.tsx
+++ front/src/navigations/root/RootNavigator.tsx
@@ -1,8 +1,16 @@
+import {useEffect} from 'react';
 import AuthStackNavigator from '../stack/AuthStackNavigator';
 import MainSearchStackNavigator from '../stack/MainSearchStackNavigator';
+import SplashScreen from 'react-native-splash-screen';
 
 function RootNavigator() {
   const isLogin = true; // 마크업 작업을 위한 임시 설정
+
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+  });
 
   return <>{isLogin ? <MainSearchStackNavigator /> : <AuthStackNavigator />}</>;
 }
